32e7a1fe83f5dc2ba273dffffe2074bf22d068c1
muhafash <me.muhfarash@gmail.com>
2022-12-02 15:16:12 +0700

    Done Lect Week 8

--- code/app.js
+++ code/app.js
@@ -1,16 +1,20 @@
-// import 
-const router = require('./routes').router;
-const log = require('./middlewares/logger');
+// import
+const router = require("./routes").router;
+const log = require("./middlewares/logger");
 
 // Inisialisasi
-const express = require('express');
-const app= express();
+const express = require("express");
+const app = express();
+const path = require("path");
 
 // membuat servers
-const port = require('./routes').port;
-const server= app.listen(port, () => {console.log(`server listening.. on port ${port}`)});
+const port = require("./routes").port;
+const server = app.listen(port, () => {
+  console.log(`server listening.. on port ${port}`);
+});
 
 // membuat routing
+app.use("/public", express.static(path.join(__dirname, "uploads"))); // rpute file static ke path url public/nama file static
 // middlewres diatas res agar berjalan
 app.use(log);
 // mengembalikan nilai body yang dikirim dari postman api
@@ -18,12 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(router);
 // handling error router
 app.use((req, res, next) => {
-    res.status(404);//tampil ke console web browser 
-    // membuat tampil ke layar
-    res.send({
-        status: "failed",
-        message: "Resource" +req.originalUrl + "Not Found"
-    });
-    next();
-    
+  res.status(404); //tampil ke console web browser
+  // membuat tampil ke layar
+  res.send({
+    status: "failed",
+    message: "Resource" + req.originalUrl + "Not Found",
+  });
+  next();
 });
--- code/routes.js
+++ code/routes.js
@@ -11,61 +11,75 @@
  * ! @patch=> Method send|yg mengirim resource|data diperbaharui|update namun secara spesific resource yg ingin diubah
  * ! @delete=> Method yang menghapus resource yg diinsialasisasi
  */
-const router = require("express").Router();//router middleware|function di express
-const multer = require("multer");// multer untuk uploads
-const upload = multer({ dest: "uploads/"});// dest = destination
-const port = process.env.PORT ? process.env.PORT : 4000;// membuat  server port
+const router = require("express").Router(); //router middleware|function di express
+const multer = require("multer"); // multer untuk uploads
+const upload = multer({ dest: "/uploads" }); // dest = destination
+const fs = require("fs"); // fs
+const path = require("path"); // path
+const port = process.env.PORT ? process.env.PORT : 4000; // membuat  server port
 
-router.get("/", (req, res)=>{
-    const {page, total}=req.query;
-    res.send({
-        status: "Success",
-        message: "Welcome to Express Tutorial",
-        page,//page = query yang dimasukkan di path
-        total// total = query yang  dimasukkan dipath
-    });//response dalam bentuk tampil html
+router.get("/", (req, res) => {
+  const { page, total } = req.query;
+  res.send({
+    status: "Success",
+    message: "Welcome to Express Tutorial",
+    page, //page = query yang dimasukkan di path
+    total, // total = query yang  dimasukkan dipath
+  }); //response dalam bentuk tampil html
 });
 /**
  * ! Penjelasan flow
  * @get => sebuah method untuk mengambil nilai yang dikirim
- * @req => meminta nilai yang dikirim|ada 
+ * @req => meminta nilai yang dikirim|ada
  * @req => dapat berupa parameter/path url, query/string
  * @res => mengembalikan nilai yang didapatkan dari @req
  */
 
-router.get("/product/:id", (req, res)=>{
-    let data = req.params;//inisialisasi variable parameter
-    res.json({
-        id: data.id
-    });//mengembalikan nilai param dalam bentuk json/ send respon json
+router.get("/product/:id", (req, res) => {
+  let data = req.params; //inisialisasi variable parameter
+  res.json({
+    id: data.id,
+  }); //mengembalikan nilai param dalam bentuk json/ send respon json
 });
-router.post("/product", (req, res)=>{
-    const {name, price, stocks, status}= req.body;//inisialisasi body 
-    res.json(req.body);//send response dalam json yang didapat dari objects body
+router.post("/product/", upload.single("image"), (req, res) => {
+  const { name, price, stocks, status } = req.body; //inisialisasi body
+  const image = req.file;
+  const checkImage = image ? console.log("ada") : console.log("ga ada"); //check ada image karna menggunakan ubuntu buth permit
+  if (image) {
+    const target = path.join(__dirname, "uploads", image.originalname); // join(direktory, target, urldikirim)
+    fs.renameSync(image.path, target);
+    // res.sendFile(target); //send response dalam bentuk file propertiesobject) static file
+  } // convert file to original
+  res.json({
+    name,
+    price,
+    stocks,
+    status,
+    image,
+  }); //send response dalam json yang didapat dari objects body
 });
 /**
  * ! Flownya
  * @post => sebuah method untuk mengupdate|mengirim sebuah nilai
  * @req => meminta nilai yang dikirim dari @post api
- * @res => mengembalikan nilai yang yang dikirim dari @post 
-*/
+ * @res => mengembalikan nilai yang yang dikirim dari @post
+ */
 
 // app.post('/productcover',upload.single("productimage") ,(req, res) => {
 // })
 
-router.get("/customer/:name", (req, res)=>{
-    let name = req.params;//inisialisasi variable
-    console.log(req.query);//check request berdasarkan query
-    res.json({name});//send response dalam json
+router.get("/customer/:name", (req, res) => {
+  let name = req.params; //inisialisasi variable
+  console.log(req.query); //check request berdasarkan query
+  res.json({ name }); //send response dalam json
 });
 
-
-router.get("/:category/:tag", (req, res)=>{
-    let {category, tag} = req.params;//inisialisasi object parameter
-    res.json({
-        category: category,
-        tag: tag
-    });//send response json dari object param
+router.get("/:category/:tag", (req, res) => {
+  let { category, tag } = req.params; //inisialisasi object parameter
+  res.json({
+    category: category,
+    tag: tag,
+  }); //send response json dari object param
 });
 
-module.exports = {router, port};//standard module exports
+module.exports = { router, port }; //standard module exports
